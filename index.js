6c4f20fb8e693b14b443ec3a387e3c35b058b285
yoshitaka motomura <stay.daybreak@gmail.com>
2020-06-28 00:56:29 +0900

    Modified

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const fs = require("fs");
 const readline = require("readline");
 const shell = require("shelljs");
 const ProgressBar = require("progress");
-
+const request = require("request");
+const extListFile =
+  "https://raw.githubusercontent.com/Yoshiturkey/vscode-ext-installer/master/extensions.txt";
 let bar = new ProgressBar("vscode extensions installed [:bar]", { total: 23 });
 const { exit } = require("process");
 
@@ -15,7 +17,21 @@ if (!isCode) {
   exit(1);
 }
 
-console.log("OK");
+async function run() {
+  await request(extListFile, (error, response, body) => {
+    if (error !== null) {
+      console.error("error:", error);
+      return false;
+    }
+    const text = body.split(/\r\n|\r|\n/);
+    text.forEach((element) => {
+      console.log(`code --install-extension ${element}`);
+      // bar.tick();
+    });
+  });
+}
+
+run();
 // const readStrem = fs.createReadStream('extensions.txt')
 
 // const interface = readline.createInterface({
