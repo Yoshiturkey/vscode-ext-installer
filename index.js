4346bdd5eb5e0d5fc31abfa89fa7e36be7bf265f
yoshitaka motomura <stay.daybreak@gmail.com>
2020-06-28 00:40:34 +0900

    WIP

--- index.js
+++ index.js
@@ -2,29 +2,27 @@
 
 const fs = require("fs");
 const readline = require("readline");
-const shell = require('shelljs');
-const ProgressBar = require('progress');
+const shell = require("shelljs");
+const ProgressBar = require("progress");
 
-let bar = new ProgressBar('vscode extensions installed [:bar]', { total: 23 });
-const {
-    exit
-} = require("process");
+let bar = new ProgressBar("vscode extensions installed [:bar]", { total: 23 });
+const { exit } = require("process");
 
-
-const isCode = shell.which('code') || false
+const isCode = shell.which("code") || false;
 
 if (!isCode) {
-    console.error('codeコマンドがありません')
-    exit(1)
+  console.error("codeコマンドがありません");
+  exit(1);
 }
 
-const readStrem = fs.createReadStream('extensions.txt')
+console.log("OK");
+// const readStrem = fs.createReadStream('extensions.txt')
 
-const interface = readline.createInterface({
-    input: readStrem
-})
+// const interface = readline.createInterface({
+//     input: readStrem
+// })
 
-interface.on('line', (lineString) => {
-   shell.exec(`code --install-extension ${lineString}`, {silent: true})
-   bar.tick();
-})
+// interface.on('line', (lineString) => {
+//    shell.exec(`code --install-extension ${lineString}`, {silent: true})
+//    bar.tick();
+// })
